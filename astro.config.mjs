de94d016e972c6d015abe73f2656eb95bffefa8d
Christopher Gantes <78065304+christophergantes@users.noreply.github.com>
2025-04-30 18:13:43 -0400

    Created basic home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.christophergantes.com",
+  site: "https://www.christophergantes.com/",
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
